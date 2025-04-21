3db4b69969f83f6f03acda6561868fb68eaad1cd
ws1023 <leeweisen80@gmail.com>
2025-04-21 16:24:14 +0800

    new change

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Ohayo, sekai!");
+        System.out.println("Bonjour, le monde!");
+    }
+
+    public String getGreeting() {
+        return "Hello, World!";
+    }
+}
